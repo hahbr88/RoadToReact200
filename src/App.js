7a3fc611063efbe1970db455e4b2c0e25c09b674
hahbr88 <90291796+hahbr88@users.noreply.github.com>
2022-08-16 01:56:47 +0900

    006-008

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import LifecycleEx from './R005_LifecycleEX'
+import LifecycleEx from './R008_LifecycleEx'   // R008_LifecycleEx.js 임포트
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <LifecycleEx></LifecycleEx>
+      <LifecycleEx
+        prop_value = 'FromApp.js'   // R008_LifecycleEx 컴포넌트로 prop_value 라는 변수 전달
+      />
     </div>
   );
 }
 
 export default App;
+
